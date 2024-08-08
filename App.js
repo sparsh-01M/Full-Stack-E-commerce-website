0583060c0e827dd3cf26ea69b1415d9a8f104998
sparsh-01M <113444357+sparsh-01M@users.noreply.github.com>
2024-08-08 17:50:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import image from './test.jpg';
+function App() {
+  return (
+    <div>
+      <img src={image} alt="Italian Trulli"/>
+      <h1>Heading 1</h1>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+      <p>This is my heading 1 and I am learning react.</p>
+    </div>
+  );
+}
+
+export default App;
